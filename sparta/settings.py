d6f5b521909a39e2f49035aabc04008b13d62a72
geang <hsegeang@gmail.com>
2022-02-15 15:54:28 +0900

    mysqlTEST

--- sparta/settings.py
+++ sparta/settings.py
@@ -9,10 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 from pathlib import Path
 from typing import List
-
 import pymysql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
